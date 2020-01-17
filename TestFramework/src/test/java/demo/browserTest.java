2e48ac3bab60d1b80c574f484fc39f2881dbc915
shyna <shyna@LAPTOP-EP6SFNCB>
2020-01-17 02:08:41 -0500

    Following Page object Model design Pattern

--- TestFramework/src/test/java/demo/browserTest.java
+++ TestFramework/src/test/java/demo/browserTest.java
@@ -8,46 +8,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.amazonSearchPage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class browserTest {
 
-	WebDriver driver;
+	private static WebDriver driver = null;
 
-public void invokeBrowser()
+public static void invokeBrowser()
 {
 
 	   //System.setProperty("webdriver.chrome.driver", "C:/Users/shyna/git/TestProject/TestFramework/drivers/chromeDriver/chromedriver.exe");
 	   //driver = new ChromeDriver();
 	   WebDriverManager.chromedriver().setup();
 	   driver = new ChromeDriver();
+	   
 	   driver.get("http://www.amazon.com");
+	   //open amazon home page
 	   driver.manage().window().maximize();
+	   //maximize screen
 	   driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	   driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-	   WebElement searchTextBox = driver.findElement(By.id("twotabsearchtextbox"));
-       searchTextBox.sendKeys("iphone 11 pro max");
-       WebElement searchButton = driver.findElement(By.xpath("//div[@class='nav-search-submit nav-sprite']//input[@class='nav-input']"));
-       searchButton.click();
-       
-       List<WebElement> listOfResults = driver.findElements(By.xpath("//span[contains(text(),'Apple iPhone 11 Pro Max, 256GB, Midnight Green, Fu')]"));
-    		  
-       //int count = listOfResults.size();
-       //System.out.println("No. of products"+ count);
-       listOfResults.get(0).click();
-       WebElement addToCartButton = driver.findElement(By.id("add-to-cart-button"));
-       addToCartButton.click();
-       driver.findElement(By.xpath("//a[@id='hlb-ptc-btn-native']")).click();
+	   
+	   amazonSearchPage.textboxSearch(driver).sendKeys("iphone 11 pro max");
+	   //find search textbox and search the data
+	   amazonSearchPage.searchButton(driver).click();
+	   //find search button and click it
+	   amazonSearchPage.searchResult(driver).get(0).click();
+	   //get the first item in list
+	   amazonSearchPage.addCart(driver).click();
+	   //add that item to cart
+	   amazonSearchPage.paymentButton(driver).click();
+	
      
-    
 }
 
 	public static void main(String[] args) 
 	{
-		browserTest object1 = new browserTest();
-     object1.invokeBrowser();
+		invokeBrowser();
 	}
 }
 
-//twotabsearchtextbox
---
+++ TestFramework/src/test/java/pages/amazonSearchPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class amazonSearchPage {
+	
+	private static WebElement element=null;
+	
+	
+	public static WebElement textboxSearch(WebDriver driver) {
+	  element = driver.findElement(By.id("twotabsearchtextbox"));
+	     return element;  
+		}
+	
+	public static WebElement searchButton(WebDriver driver) {
+		 element = driver.findElement(By.xpath("//div[@class='nav-search-submit nav-sprite']//input[@class='nav-input']"));
+		  return element;  
+			}
+
+	public static List<WebElement> searchResult(WebDriver driver) {
+		List<WebElement> element = driver.findElements(By.xpath("//span[contains(text(),'Apple iPhone 11 Pro Max, 256GB, Midnight Green, Fu')]"));
+	    return element;  
+			}
+	
+	public static WebElement addCart(WebDriver driver) {
+		 element = driver.findElement(By.id("add-to-cart-button"));
+	    return element;  
+			}
+	
+	public static WebElement paymentButton(WebDriver driver) {
+		 element = driver.findElement(By.xpath("//a[@id='hlb-ptc-btn-native']"));
+	    return element;  
+			}
+	
+}
+
+
