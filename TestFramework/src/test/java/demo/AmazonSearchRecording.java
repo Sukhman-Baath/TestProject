c52ca963d9bf77a1404fc9df8aff6bec9abdeec6
shyna <shyna@LAPTOP-EP6SFNCB>
2020-01-20 20:08:02 -0500

    recording tests and importing to project using katalon recorder

---
+++ TestFramework/src/test/java/demo/AmazonSearchRecording.java
@@ -0,0 +1,81 @@
+package demo;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AmazonSearchRecording {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+	WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testRecordingDemo() throws Exception {
+    driver.get("https://www.amazon.com/");
+    driver.findElement(By.id("twotabsearchtextbox")).click();
+    driver.findElement(By.id("twotabsearchtextbox")).clear();
+    driver.findElement(By.id("twotabsearchtextbox")).sendKeys("when i was you");
+    driver.findElement(By.xpath("//input[@value='Go']")).click();
+    driver.findElement(By.xpath("//div[@id='search']/div/div[2]/div/span[4]/div/div/div/span/div/div/div[2]/div[2]/div/div/div/div/div/h2/a/span")).click();
+    driver.findElement(By.name("submit.preorder")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
